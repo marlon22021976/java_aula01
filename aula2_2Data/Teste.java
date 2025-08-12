3d9b85107c9f84b5826da13b048611678fa20e31
marlon22021976 <marlon.costa@aluno.fmpsc.edu.br>
2025-08-12 09:45:08 -0300

    Delete aula2_2Data directory

--- aula2_2Data/Teste.java
+++
@@ -1,7 +0,0 @@
-public class Teste{
-    public static void main (String[] args){
-        Data dataNascimento = new Data(12, 5, 1990);
-        Pessoa pessoa = new Pessoa("Ivan", dataNascimento);
-        pessoa.exibirInformacoes();
-    }
-}
